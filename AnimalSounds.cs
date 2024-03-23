a4e55bd43adf990d3a6295bc78f5ba9cf57efdda
Teodora Moldovan <teodoraioana.moldovan@ulbsibiu.ro>
2024-03-23 11:47:10 +0200

    Add project files.

---
+++ AnimalSounds.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimalSounds
+{
+    public class AnimalManager
+    {
+        // Dictionar utilizat pentru asocierea animalelor si a sunetelor 
+        private Dictionary<string, string> animalSounds = new Dictionary<string, string>();
+
+
+        // Adaugarea unui nou animal in dictionar
+        public void AddAnimal(string type, string sound)
+        {
+            animalSounds[type] = sound;
+        }
+
+        // Returnarea tuturor elementelor din dictionar
+        public Dictionary<string, string> GetAnimalSounds()
+        {
+            return animalSounds;
+        }
+
+        // Metoda pentru a obtine toate tipurile de animale - cheile dictionarului
+        public IEnumerable<string> GetAnimalTypes()
+        {
+            return animalSounds.Keys;
+        }
+
+
+        // Metoda pentru a obtine sunetele animalelor
+        public string GetSound(string type)
+        {
+            if (animalSounds.ContainsKey(type))
+                return animalSounds[type];
+            else
+                return "Sunetul nu a fost inregistrat";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AnimalSounds;
+
+namespace Tema1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalManager animalManager = new AnimalManager();
+            animalManager.AddAnimal("Caine", "Ham");
+            animalManager.AddAnimal("Pisica", "Miau");
+            animalManager.AddAnimal("Vaca", "Muu");
+            animalManager.AddAnimal("Pui", "Piu");
+            animalManager.AddAnimal("Rata", "Mac");
+            animalManager.AddAnimal("Leu", "Roar");
+
+            Console.WriteLine("Sunete de animale\n");
+            foreach (var animalType in animalManager.GetAnimalTypes())
+            {
+                Console.WriteLine($"{animalType} face sunetul: {animalManager.GetSound(animalType)}");
+            }
+
+            string soundOfElephant = animalManager.GetSound("Elefant");
+            Console.WriteLine($"Elefantul face sunetul: { soundOfElephant}");
+
+            Console.ReadKey();
+        }
+    }
+}
