03666630f264f890ed0cb7095eeb6f37ef18691b
Teodora Moldovan <teodoraioana.moldovan@ulbsibiu.ro>
2024-04-08 18:14:42 +0300

    A separate method to populate the animal sounds in the animal manager and a separate method to display the animal sounds

--- Program.cs
+++ Program.cs
@@ -8,23 +8,33 @@ namespace Tema1
         static void Main(string[] args)
         {
             AnimalManager animalManager = new AnimalManager();
+            PopulateAnimalSounds(animalManager);
+
+            Console.WriteLine("Sunete de animale");
+            DisplayAnimalSounds(animalManager);
+            
+            string soundOfElephant = animalManager.GetSound("Elefant");
+            Console.WriteLine($"Elefantul face sunetul: { soundOfElephant}");
+
+            Console.ReadKey();
+        }
+
+        static void PopulateAnimalSounds(AnimalManager animalManager)
+        {
             animalManager.AddAnimal("Caine", "Ham");
             animalManager.AddAnimal("Pisica", "Miau");
             animalManager.AddAnimal("Vaca", "Muu");
             animalManager.AddAnimal("Pui", "Piu");
             animalManager.AddAnimal("Rata", "Mac");
             animalManager.AddAnimal("Leu", "Roar");
+        }
 
-            Console.WriteLine("Sunete de animale\n");
+        static void DisplayAnimalSounds(AnimalManager animalManager)
+        {
             foreach (var animalType in animalManager.GetAnimalTypes())
             {
                 Console.WriteLine($"{animalType} face sunetul: {animalManager.GetSound(animalType)}");
             }
-
-            string soundOfElephant = animalManager.GetSound("Elefant");
-            Console.WriteLine($"Elefantul face sunetul: { soundOfElephant}");
-
-            Console.ReadKey();
         }
     }
 }
